a6d302e02b63eccfb8c96e978866020cfb23db8c
tkmxqrd <tkmxqrd@gmail.com>
2024-09-13 19:43:14 +0200

    Remake of build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
+# Run cargo install
 cargo install --path .
+
+# Move the binary to /usr/bin/ after installation
+sudo mv /home/$USER/.cargo/bin/logviewer /usr/bin/
+
+# Check the user's shell
+if [ "$SHELL" == "/bin/bash" ]; then
+    # Restart bash shell
+    exec bash
+elif [ "$SHELL" == "/bin/zsh" ]; then
+    # Restart zsh shell
+    exec zsh
+else
+    echo "Unsupported shell: $SHELL"
+fi
