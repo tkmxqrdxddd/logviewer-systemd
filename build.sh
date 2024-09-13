0a9ed52ef4194e6e07d7802946cd2934257a8456
Tkmxqrd <tkmxqrd@gmail.com>
2024-09-13 20:41:42 +0200

    edit build.sh

--- build.sh
+++ build.sh
@@ -6,13 +6,3 @@ cargo install --path .
 # Move the binary to /usr/bin/ after installation
 sudo mv /home/$USER/.cargo/bin/logviewer /usr/bin/
 
-# Check the user's shell
-if [ "$SHELL" == "/bin/bash/" ]; then
-    # Restart bash shell
-    exec bash
-elif [ "$SHELL" == "/bin/zsh/" ]; then
-    # Restart zsh shell
-    exec zsh
-else
-    echo "Unsupported shell: $SHELL"
-fi
