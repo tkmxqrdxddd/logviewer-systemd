6070724878256fc2f4b75322236ea341ec11b3d1
Tkmxqrd <tkmxqrd@gmail.com>
2024-09-13 18:45:56 +0200

    build.sh

--- build.sh
+++ build.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
 
-# Build the Rust program
-cargo build --release
-
-# Check if the build was successful
-if [ $? -eq 0 ]; then
-    # Move the compiled binary to /usr/local/bin
-    sudo mv target/release/logviewer /usr/local/bin/logviewer
-    echo "Logviewer binary created at /usr/local/bin/logviewer"
-else
-    echo "Build failed. Check the error messages for more details."
-fi
-
-# Make the script executable
-chmod +x build.sh
+cargo install --path .
