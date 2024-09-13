bc0b5f074cf0ed5a9eec4c04eaf68b49e91de940
tkmxqrd <tkmxqrd@gmail.com>
2024-09-13 17:51:22 +0200

    some changes made, no biggie

--- build.sh
+++ build.sh
@@ -6,8 +6,8 @@ cargo build --release
 # Check if the build was successful
 if [ $? -eq 0 ]; then
     # Move the compiled binary to /usr/local/bin
-    sudo mv target/release/logmaster /usr/local/bin/logmaster
-    echo "Logmaster binary created at /usr/local/bin/logmaster"
+    sudo mv target/release/logviewer /usr/local/bin/logviewer
+    echo "Logviewer binary created at /usr/local/bin/logviewer"
 else
     echo "Build failed. Check the error messages for more details."
 fi
